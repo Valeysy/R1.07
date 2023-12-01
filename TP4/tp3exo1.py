278e253e1f703a2fb37f63f3e47465cae6022fd7
Vladimir Nechaev <99259679+Valeysy@users.noreply.github.com>
2023-12-01 11:46:47 +0100

    Delete TP4 directory

--- TP4/tp3exo1.py
+++
@@ -1,8 +0,0 @@
-ask_nb = float(input("Vous cherchez la table de multiplication de quel nombre ? "))
-table = []
-
-for multiply in range(10):
-    table.append(round(ask_nb * multiply, 1))  
-    
-for multiply in range(10):
-    print(f"{ask_nb} * {multiply} = {table[multiply]}")
